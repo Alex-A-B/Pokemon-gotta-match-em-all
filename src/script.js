a49476ed4af1672cf092eb1239fd0c12d50eb52c
Alex-A-B <alexjamesbelton@gmail.com>
2021-06-02 12:35:06 +0100

    minor change to data get, add some comments for clarity

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+// test linkages 
+
 const para = document.createElement('p')
-para.innerText = "Added to test linkages"
+para.innerText = "General Kenobi"
 document.getElementById("start").appendChild(para)
+
+// test fetch and console log
+
+fetch("https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id")
+.then(response => response.json())
+.then(obj => console.log(obj.data))
+
+
+// render images
