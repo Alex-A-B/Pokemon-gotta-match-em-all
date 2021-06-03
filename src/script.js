c699263fc661b97101c4ee33f9d3e2575e11c028
Alex-A-B <alexjamesbelton@gmail.com>
2021-06-03 21:56:11 +0100

    re-ordered initial functions

--- src/script.js
+++ src/script.js
@@ -1,14 +1,34 @@
-// test linkages 
-
-// const para = document.createElement("p")
-// para.innerText = "General Kenobi"
-// document.getElementById("start").appendChild(para)
-
 // fetch() code and console log
 
+/*  variable to have the Base URL for the fetch - prevent typos! */
 const BASEURL = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
-let fillPokedex = (pokemon) => {
+/*  function to set board size which is #pairs of chars to match *
+ *  Will be looking to set some repeat protection via if in the  *
+ *  loop. Seek to increase the board size for #pairs via toggle  */
+const setPokemonArray = function(pokedexArray) {
+    const pokemonArray = [];
+    for (let i = 0; i < 8; i++) {
+        let num = randomPokemonId();
+        let pokemon = pokedexArray.results[num];
+        pokemonArray.push(pokemon)
+    }
+    const gameArray = [...pokemonArray, ...pokemonArray]  
+    fillGameBoard(gameArray)
+}
+
+/* get random number function for the gameBoard cards */
+let randomPokemonId = () => {
+    return Math.floor(Math.random() * 151)
+}
+
+/* function to take the game array and  */
+const fillGameBoard = function(pokeArray){
+    pokeArray.forEach(pokemon => fillPokedex(pokemon))
+}
+/* function to create the cards based on initial fetch info, which *
+ * is then worked to create the gameBoard with the cards in play.  */
+const fillPokedex = (pokemon) => {
     let pokeURL = pokemon.url
     fetch(pokeURL)    
     .then(response => response.json())
@@ -16,37 +36,18 @@ let fillPokedex = (pokemon) => {
     .then(pokemon => renderCards(pokemon)) 
 }
 
-const pokemonArray = [];
-
-const fillGameBoard = function(pokeArray){
-    pokeArray.forEach(pokemon => fillPokedex(pokemon))
-}
-
-let randomPokemonId = () => {
-    return Math.floor(Math.random() * 151)
-}
-
-
-const setPokemonArray = function(pokedexArray) {
-        for (let i = 0; i < 8; i++) {
-        let num = randomPokemonId();
-        let pokemon = pokedexArray.results[num];
-        pokemonArray.push(pokemon)
-    }
-    let gameArray = [...pokemonArray, ...pokemonArray]  
-    fillGameBoard(gameArray)
-}
-
+/* need to add this to a function rather than just on load, so it  *
+ * can be added to a restart/new game button.                      */
 fetch(BASEURL)
 .then(response => response.json())
 // .then(pokedex => pokedex.results.forEach(pokemon => console.log(pokemon)))
 .then(pokedex => setPokemonArray(pokedex))
 
 
-// check console log - expect ID, title and image ID object.
-
 const gameBoard = document.querySelector(".gameBoard");
 
+/* card render function - builds the cards for the game. Scaled back *
+ * for MVP - may need to add an ID# for logic checking when matching */
 // render cards
 const renderCards = function(pokedex) {
 // card
@@ -57,18 +58,18 @@ const createCardFront = document.createElement("div")
 createCardFront.className = "card-front"
 // front elements - name, image, info panel
 const cardHeader = document.createElement("h2")
-cardHeader.innerText = `${pokedex.species.name.toUpperCase()}`                  /* to get from data api */
+cardHeader.innerText = `${pokedex.species.name.toUpperCase()}`                 
 createCardFront.appendChild(cardHeader)
 
 const cardImage = document.createElement("img")
 cardImage.className = "card-image"
-cardImage.src = pokedex.sprites.other[`official-artwork`].front_default         /* to get from data api */
+cardImage.src = pokedex.sprites.other[`official-artwork`].front_default        
 createCardFront.appendChild(cardImage)
 
 /* not needed for MVP */
 // const cardInfo = document.createElement("p")
 // cardInfo.className = "card-info"
-// cardInfo.innerText = "some interesting facts about the image"    /* to get from data api */
+// cardInfo.innerText = "some interesting facts about the image"                
 // createCardFront.appendChild(cardInfo)
 
 // card back
